bf6cdaef387ed26daac7331eab67fc43421181b2
Emma Paris <wkenneddy505@gmail.com>
2022-12-11 15:46:30 -0300

    refactor: remove console log

--- src/math/calculateDistanceBetweenPoints.ts
+++ src/math/calculateDistanceBetweenPoints.ts
@@ -5,7 +5,6 @@ export function calculateDistanceBetweenPoints(p1: Point, p2: Point) {
   const { x: x2, y: y2 } = p2;
   const x = Math.pow(x2 - x1, 2);
   const y = Math.pow(y2 - y1, 2);
-  console.log(x, y);
   const distance = Math.round(Math.sqrt(x + y));
   return distance;
 }
